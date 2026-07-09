424a28802d7c2342d2e7c599cd53f9137ff46e30
rathodjagdish694-dot <rathodranjana694@gmail.com>
2026-07-09 18:05:15 +0530

    Add files via upload

---
+++ Perimeter.c
@@ -0,0 +1,28 @@
+// Name- Jagdish Rameshwar Rathod 
+// PRN- 2503033111372012
+
+#include <stdio.h>
+int main(){
+	int width,breadth,perimeter;
+	
+	
+	printf("Enter width:\t");
+	
+	
+	scanf("%d",&width);
+	
+	
+	printf("Enter breadth:\t");
+	
+	
+	scanf("%d",&breadth);
+	
+	
+	perimeter=(2*width+2*breadth) ;
+	
+	
+	printf("perimeter of Rectangle is %d",perimeter);
+	
+	
+	return 0;
+}
---
+++ basic calculations.c
@@ -0,0 +1,25 @@
+// Name- Jagdish Rameshwar Rathod 
+//PRN- 2503033111372012
+#include<stdio.h>
+int main()
+{
+	int num1,num2,sum,sub,mul,div;
+	printf("Enter number1:\t");
+	scanf("%d",&num1);
+	printf("Enter number2:\t");
+	scanf("%d",&num2);
+	sum=num1+num2;
+	printf("addition of %d & %d is\t %d",
+	num1,num2,sum);
+	sub=num1-num2;
+	printf("\nsubtraction of %d & %d is\t %d",
+	num1,num2,sub);
+	mul=num1*num2;
+	printf("\nmultiplication of %d & %d is\t %d",
+	num1,num2,mul);
+	div=num1/num2;
+	printf("\ndivision of %d & %d is\t %d",
+	num1,num2,div);
+	return 0;
+	}
+   	  
